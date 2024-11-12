65c975ae2fc3273d50219f934c11824c4c964065
Rockybhai-cool <coolcriminal786@gmail.com>
2024-11-12 19:47:31 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,12 +6,12 @@ class Config(object):
 
     BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", "27317669"))
 
-    API_HASH = os.environ.get("API_HASH", "")    
+    API_HASH = os.environ.get("API_HASH", "11b88c331c5d44fde57cf91de1a2156b")    
     
-    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "Real_Pirates")
 
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
 
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7326600267").split())
